17bc399b7f9bfcdd8b4b51f06c40b861b9a52f50
tamya <tamyacode@qq.com>
2021-08-17 09:34:40 +0800

    隐藏浏览器特征

---
+++ Common/browserScripts/testCase.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+# 加载该文件后，隐藏特征
+with open('./file/stealth.min.js') as f:
+    js = f.read()
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+  "source": js
+})
+
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+
+driver.close()
