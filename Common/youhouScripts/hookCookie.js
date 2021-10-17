8be8aaa104bbf7391eead4669502f389092ff3c2
tamya <tamyacode@qq.com>
2021-10-17 23:03:08 +0800

    完善hook cookie方式

--- Common/youhouScripts/hookCookie.js
+++ Common/youhouScripts/hookCookie.js
@@ -46,3 +46,27 @@ Object.defineProperty(document, 'cookie', {
 });
 
 document.cookie = old_cookie;
+
+// 方式二完善版：
+var cookie_cache = document.cookie;
+
+Object.defineProperty(document, 'cookie', {
+    get: function() {
+        console.log('Getting cookie');
+        return cookie_cache;
+    },
+    set: function(val) {
+        console.log("Seting cookie",val);
+        var cookie = val.split(";")[0];
+        var ncookie = cookie.split("=");
+        var flag = false;
+        var cache = cookie_cache.split("; ");
+        cache = cache.map(function(a){
+            if (a.split("=")[0] === ncookie[0]){
+                flag = true;
+                return cookie;
+            }
+            return a;
+        })
+    }
+})
